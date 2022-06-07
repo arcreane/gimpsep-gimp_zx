31bc0c061e469c4ba1d28bf5353b86553c3e2e2f
jiemingxuxuxu <74723376+jiemingxuxuxu@users.noreply.github.com>
2022-06-07 11:37:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,8 +62,30 @@ int main()
 	waitKey();
 	destroyAllWindows();
 	//return 0;
-	//%%3.Lighten_Darken%%//
+	
+	//%%3.Lighten_Darken
 
+	int g_slider1_pos = 10;
+	int g_slider2_pos = 0;
+	const char* windowName = "Contrast And Brightness";
+
+	Mat srcImage = imread("eco.jpg");
+	if (!srcImage.data) {
+		cerr << "fail to load image" << endl;
+		return -1;
+	}
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	createTrackbar("Contrast", windowName, &g_slider1_pos, 300, ContrastAndBright,
+		(void*)&srcImage); //对比度
+	createTrackbar("Brightness", windowName, &g_slider2_pos, 100,
+		ContrastAndBright, (void*)&srcImage); //亮度
+
+	ContrastAndBright(g_slider1_pos, (void*)&srcImage);
+	ContrastAndBright(g_slider2_pos, (void*)&srcImage);
+	imshow(windowName, srcImage);
+	waitKey();
+	destroyAllWindows();
+	return 0;
 	//%%4.Panorama%%//
 	Mat img1 = imread("KamenriderREVI.jpg");
 	Mat img2 = imread("KamenriderVICE.jpg");
